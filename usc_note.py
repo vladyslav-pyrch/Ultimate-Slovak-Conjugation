634ab27b5113b39864ad6ff999822623d742b2ee
Vlad Pyrch <vlad.nuron@gmail.com>
2023-05-26 02:54:01 +0300

    Updated USCNote

--- usc_note.py
+++ usc_note.py
@@ -5,9 +5,6 @@ from common import USCModel
 
 class USCNote(genanki.Note):
     @staticmethod
-    def create(uuid: str, prompt: str, notes: list[str], tags: list[str],
-               similar: list[str] | None = None) -> "USCNote":
-        similar: str = "" if similar is None else "".join(similar)
-        notes: str = "".join(notes)
-
+    def create(uuid: str, prompt: str, notes: str, tags: list[str],
+               similar: str | None = None) -> "USCNote":
         return USCNote(model=USCModel, fields=[uuid, prompt, similar, notes], tags=tags, guid=uuid)
